f99d9c2261277c87fd0a47d91c3c1c170910e0bd
cstielper <chris@mm4solutions.com>
2016-12-09 14:03:05 -0500

    Added JS code

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
-
+	$.ajax({
+		url: 'https://www.codeschool.com/users/cstielper.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			console.log(response.courses.completed);
+			for(var i = 0; i < response.courses.completed.length; i++) {
+				var course = $('<div class="course">');
+				$('#badges').append(course);
+				$(course).append('<h3 title="' + response.courses.completed[i].title + '">' + response.courses.completed[i].title + '</h3>');
+				$(course).append('<img src="' + response.courses.completed[i].badge + '">');
+				$(course).append('<a href="' + response.courses.completed[i].url + '" target="_blank" class="btn btn-primary">' + response.courses.completed[i].title + '</a>');
+			}
+		}
+	});
 });
